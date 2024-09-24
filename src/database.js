c74d9b923e5274249296dd88c91942a2f1fe09dc
Daniel <86085164+msck1@users.noreply.github.com>
2024-09-24 12:28:32 -0300

    Tentando criar database

--- src/database.js
+++ src/database.js
@@ -1,34 +1,54 @@
+import "dotenv/config"
 import { ChromaClient } from "chromadb";
 import { Chroma } from "@langchain/community/vectorstores/chroma"
 import { OpenAIEmbeddings } from "@langchain/openai"
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory"
+import { TextLoader } from "langchain/document_loaders/fs/text";
 
+const loader = new DirectoryLoader("../md",{
+        ".txt": (path) => new TextLoader (path),
+    },
+);
 
+const docs = await loader.load();
 
-
-const embeddings = new OpenAIEmbeddings({
-    model: "text-embedding-3-large",
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+    
 });
 
+const chunks = await splitter.splitDocuments(docs);
 
+console.log(chunks)
 
-// const client = new ChromaClient();
+console.log(docs)
 
-// const collection = await client.createCollection({
-//     name: "teste2",
-// });
+console.log(splitter)
 
-// await collection.add({
-//     documents: [
-//         "Documento sobre carros",
-//         "Documento sobre maças"
-//     ],
-//     ids: ["id1", "id2"],
-// })
+eddings( chunks, { // faz o embedding
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  model: "text-embedding-3-large",
+  azureOpenAIApiKey: "",
+})
 
-// const results = await collection.query({
-//     queryTexts: "Carros",
-//     nResults: 2,
-// });
+const vectorStore = new ChromaClient() // cria cliente chroma
 
-// console.log(results);
+await vectorStore.deleteCollection({ // deleta cliente chroma
+  name: "colecaocerta"
+})
 
+const collection = await vectorStore.createCollection({ // cria coleçao
+  name: "colecaocerta",
+  metadata: {
+    "descricao": "primeira_colecao"
+  },
+  embeddingFunction: embeddings
+})
+
+const collections = await vectorStore.listCollections() // lista todas coleçoes
+
+console.log(collections)
+
+console.log(collection)
